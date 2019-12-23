ad60839612a7f8029d372f828848d084cba60a04
Fethienv <50912478+Fethienv@users.noreply.github.com>
2019-12-23 20:14:37 +0100

    Add files via upload

---
+++ plugin_registeration.php
@@ -0,0 +1,13 @@
+<?php
+
+/* 
+ * 
+ */
+
+
+
+
+// Activation Hooks
+// register_activation_hook(__FILE__, array('JAWC', 'activate_jawc'));
+// Deactivation Hooks
+// register_deactivation_hook(__FILE__, array('JAWC', 'deactivate_jawc'));
