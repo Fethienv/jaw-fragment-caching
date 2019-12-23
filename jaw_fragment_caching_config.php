bd001f3d7bdb62001240c919559b2e44dfcdcfa2
Fethienv <50912478+Fethienv@users.noreply.github.com>
2019-12-23 23:04:02 +0100

    Update jaw_fragment_caching_config.php

--- jaw_fragment_caching_config.php
+++ jaw_fragment_caching_config.php
@@ -12,12 +12,13 @@ define('JAW_VERSION', '1.0.0');
 
 define('JAW_SERVER_URL', 'https://www.jawlatte.com');
 
-
+global $wpdb,$table_prefix;
 if (!defined('JAW_Dependencies') || !JAW_Dependencies) {
     add_action('admin_notices', 'jaw_inactive_notice');
     define('FRAGMENT_CACHING_STATUS', FALSE); // repaire this
 } else{
-    define('FRAGMENT_CACHING_STATUS', TRUE); // repaire this
+    $CNF_STATUS = $wpdb->get_var('SELECT DISTINCT option_value FROM '.$table_prefix.'fragment_caching WHERE option_name = "status"');
+    define('FRAGMENT_CACHING_STATUS', $CNF_STATUS); // repaire this
 }
 
 define('FRAGMENT_DIR', ABSPATH . 'wp-content/cache/jawc-fragments-caching/');
@@ -27,10 +28,9 @@ define('FRAGMENT_DURATION', FALSE);
 
 /// EXPIRATION table
 $EXPIRATION_constants = array(
-                               'JAW_RARLY'         => 120,  // get duration from data base
+                               'JAW_RARLY'         => $wpdb->get_var('SELECT DISTINCT option_value FROM '.$table_prefix.'fragment_caching WHERE option_name = "JAW_RARLY"'),  // get duration from data base
                                'JAW_PERSISTANT'    => 0,
-                               'JAW_SPECIFIC_1'    => MONTH_IN_SECONDS,  // get duration from data base
-                               'JAW_SPECIFIC_1'    => MONTH_IN_SECONDS,  // get duration from data base
-                               'JAW_SPECIFIC_3'    => MONTH_IN_SECONDS   // get duration from data base
+                               'JAW_SPECIFIC_1'    => $wpdb->get_var('SELECT DISTINCT option_value FROM '.$table_prefix.'fragment_caching WHERE option_name = "JAW_SPECIFIC_1"'),
+                               'JAW_SPECIFIC_2'    => $wpdb->get_var('SELECT DISTINCT option_value FROM '.$table_prefix.'fragment_caching WHERE option_name = "JAW_SPECIFIC_2"'),
+                               'JAW_SPECIFIC_3'    => $wpdb->get_var('SELECT DISTINCT option_value FROM '.$table_prefix.'fragment_caching WHERE option_name = "JAW_SPECIFIC_3"'),
                              );
-
