5df656da4fd028a84c3a7143cc3cf9468313cb2b
Fethienv <50912478+Fethienv@users.noreply.github.com>
2019-12-30 01:04:42 +0100

    Update jaw_core_functions.php

--- jaw_core_functions.php
+++ jaw_core_functions.php
@@ -329,12 +329,12 @@ function jaw_remove_cache_fragments($cleanup_paths) {
  * @since    1.0.0
  * @param    mixte               $unique        enable or disable this option.
  */
-function jaw_get_user_suffix($unique = false) {
-    global $current_user;
+function jaw_get_user_suffix($unique = false) {      
     if($unique === true && is_user_logged_in()){
         $user = (current_user_can('manage_options')) ? "admin" : "user";
     } elseif($unique == "role" && is_user_logged_in()) {
-        $user = $current_user->roles[0];
+        $current_user = (!isset($GLOBALS['current_user']) || empty($GLOBALS['current_user']))?wp_get_current_user():$GLOBALS['current_user'];
+        $user = (current_user_can('manage_options')) ? "admin" :$current_user->roles[0];
     } elseif($unique == "id" && is_user_logged_in()) {
         $user = get_current_user_id();
     }else{
